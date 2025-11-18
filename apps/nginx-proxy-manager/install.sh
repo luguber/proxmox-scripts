c60d8986e2e57c28f31caffc608095da62f9dbf1
luguber <seollestad@gmail.com>
2025-11-18 21:39:31 +0100

    Update install.sh

--- apps/nginx-proxy-manager/install.sh
+++ apps/nginx-proxy-manager/install.sh
@@ -280,7 +280,8 @@ step_start "Frontend" "Building" "Built"
   export NODE_ENV=development
   yarn cache clean --silent --force >$__OUTPUT
   yarn install --silent --network-timeout=30000 >$__OUTPUT 
-  yarn build >$__OUTPUT 
+  NODE_OPTIONS="--openssl-legacy-provider --max-old-space-size=4096" yarn build > $__OUTPUT
+ # yarn build >$__OUTPUT 
   cp -r dist/* /app/frontend
  # cp -r app-images/* /app/frontend/images
 
